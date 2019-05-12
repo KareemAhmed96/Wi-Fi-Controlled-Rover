b958f69a3917c7357dc53253936a58cdb411c2ba
Kareem Ahmed <50543444+KareemAhmed96@users.noreply.github.com>
2019-05-12 16:38:40 +0200

    Update Client_Code.py

--- Client_Code.py
+++ Client_Code.py
@@ -8,10 +8,10 @@ port = 333
 
 clientsocket.connect((host, port))
 
-Motor_L_ClockWise		= machine.Pin(5, machine.Pin.OUT)
+Motor_L_ClockWise	= machine.Pin(5, machine.Pin.OUT)
 Motor_L_C_ClockWise 	= machine.Pin(4, machine.Pin.OUT) 
 
-Motor_R_ClockWise		= machine.Pin(0, machine.Pin.OUT)
+Motor_R_ClockWise	= machine.Pin(0, machine.Pin.OUT)
 Motor_R_C_ClockWise 	= machine.Pin(2, machine.Pin.OUT)
 
 while True:
